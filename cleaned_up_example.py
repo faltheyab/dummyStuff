e6c90d971707afc94e64fb572b2520ec8487a26c
faltheyab <69789293+faltheyab@users.noreply.github.com>
2025-05-11 18:18:37 +0300

    ggffgdfgdfgdfgfdgdfgdf

--- cleaned_up_example.py
+++ cleaned_up_example.py
@@ -3,7 +3,7 @@ from pyspark.sql.types import *
 from datetime import datetime, timedelta
 import os, sys, time,random
 
-finish_time=(datetime.now() + timedelta(hours=1,minutes=5))
+finish_time=(datetime.now() + timedelta(minutes=45))
 
 def get_random_int():
     return random.randint(0, 10)
@@ -19,11 +19,9 @@ spark = SparkSession.builder.appName("service").enableHiveSupport().getOrCreate(
 spark.conf.set('spark.sql.caseSensitive', True)
 spark.conf.set('spark.sql.files.ignoreMissingFiles', True)
 spark.conf.set("spark.sql.session.timeZone", "Asia/Tokyo")
-#------
-spark.conf.set("spark.hadoop.fs.s3a.connection.maximum", "64")
-spark.conf.set("spark.hadoop.fs.s3a.threads.max", "64")
 #-----
 sc = spark.sparkContext
+jvm = sc._jvm
 hconf=sc._jsc.hadoopConfiguration()
 hconf.set("fs.cos.cloudobjectstorage.endpoint", "s3.jp-tok.cloud-object-storage.appdomain.cloud")
 hconf.set("fs.cos.cloudobjectstorage.access.key", "bf49816e1eec4368a38b5dc9c57de9a2") 
@@ -33,15 +31,13 @@ hconf.set("fs.stocator.scheme.list", "cos")
 hconf.set("fs.stocator.cos.scheme", "cos")
 hconf.set("fs.cos.impl", "com.ibm.stocator.fs.ObjectStoreFileSystem")#
 hconf.set("fs.stocator.cos.impl", "com.ibm.stocator.fs.cos.COSAPIClient")
-hconf.set("fs.s3a.connection.maximum", "64")
-hconf.set("fs.s3a.threads.max", "64")
+
 log("************----------------------Done COS configuration----------------------------************")
 query = "SELECT ID, DATA FROM aetemptable LIMIT 100000"
 
 def read_write_app(input_name, output_name):
     log("\nSTEP1\n")
     read_df = spark.read.option("header", True).csv("cos://faisalsbkt.cloudobjectstorage/multiple_csvs/"+input_name)
-    read_df.count() 
 
     log("\nSTEP2\n")
     read_df.createOrReplaceTempView("aetemptable")
@@ -52,10 +48,6 @@ def read_write_app(input_name, output_name):
 
     log("\nSTEP4\n")
     query_df.repartition(1).write.option("header", True).option("compression", "gzip").mode("overwrite").csv("cos://faisalsbkt.cloudobjectstorage/output/"+output_name)
-
-    spark.catalog.dropTempView("aetemptable")
-    query_df.unpersist()
-    read_df.unpersist()
 
 
 while True:
@@ -72,7 +64,8 @@ while True:
 
 
 log("************----------------------Application finished----------------------------************")
-log("Stopping: spark context -> spark session -> sleep 10 seconds -> sys.exit(0) -> os._exit(0)")
+log("Stopping: jvm.org.apache.hadoop.fs.FileSystem.closeAll()-> spark context -> spark session -> sleep 10 seconds -> sys.exit(0) -> os._exit(0)")
+jvm.org.apache.hadoop.fs.FileSystem.closeAll()
 sc.stop()
 spark.stop()
 time.sleep(10)
